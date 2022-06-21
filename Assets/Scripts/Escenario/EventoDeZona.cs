9ea554426222c65808e9151b82f85cc270daaeb9
9RACHA <mgl15r@gmail.com>
2022-06-21 18:32:14 +0200

    Entrega del juego ya comenzado

---
+++ Assets/Scripts/Escenario/EventoDeZona.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class EventoDeZona : IListaAcciones {
+    
+    [Header("CONDICIONES")]
+    public Logro logroDisparadorDelAcontecimiento;
+}
---
+++ Assets/Scripts/ObjetosyNPCs/EquipoPokemonEntrenador.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipoPokemonEntrenador : MonoBehaviour {
+
+    [HideInInspector]
+    public List<PokemonEntrenador> pokemons;
+}
